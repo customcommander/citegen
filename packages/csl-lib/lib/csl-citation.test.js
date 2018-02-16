33130dd127c34b674e53f37dd5506063beac3ff8
customcommander <hello@spinjs.com>
2018-02-16 10:29:41 +0000

    csl-lib: use tap instead of tape

--- packages/csl-lib/lib/csl-citation.test.js
+++ packages/csl-lib/lib/csl-citation.test.js
@@ -1,7 +1,12 @@
-var test = require('tape');
+var tap = require('tap');
 var citation = require('./csl-citation');
 
-test('it should return a citation', t => {
-  t.is(citation({}, {}), 'this is a citation');
+tap.test('it should return a citation', t => {
+  t.is(citation({}, {}), 'this is a citationx');
+  t.end();
+});
+
+tap.test('it should throw if there are no locales', t => {
+  t.throws(() => citation({}));
   t.end();
 });
