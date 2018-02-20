20de9cc7c45ebde397f5f7fad1bff697557bf908
customcommander <hello@spinjs.com>
2018-02-20 01:52:03 +0000

    csl-layout: move all data transformation into a single pipeline

--- packages/csl-lib/lib/csl-layout.js
+++ packages/csl-lib/lib/csl-layout.js
@@ -3,27 +3,16 @@ var formattingAttr = require('./csl-attr-formatting');
 var affixesAttr = require('./csl-attr-affixes');
 var delimiterAttr = require('./csl-attr-delimiter');
 
-function applyAttributes(attrs, results) {
+function layout(attrs, children, refs) {
   return R.pipe(
-    delimiterAttr(attrs),
-    formattingAttr(attrs),
-    affixesAttr(attrs)
-  )(results);
-};
-
-function applyChildren(children, refs) {
-  var transducer = R.compose(
     R.map(R.of),
     R.map(R.ap(children)),
     R.map(R.join('')),
-    R.map(R.ifElse(R.isEmpty, R.always([]), R.of))
-  );
-  return R.transduce(transducer, R.concat, [], refs);
-}
-
-function layout(attrs, children, refs) {
-  var results = applyChildren(children, refs);
-  return applyAttributes(attrs, results);
+    R.filter(R.complement(R.isEmpty)),
+    delimiterAttr(attrs),
+    formattingAttr(attrs),
+    affixesAttr(attrs)
+  )(refs);
 };
 
 module.exports = function (children, attrs) {
