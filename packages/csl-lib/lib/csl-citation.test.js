d2e764e458166409c35fbb0257530c051c4a7af9
customcommander <hello@spinjs.com>
2018-02-22 14:52:21 +0000

    add support for a macros object in csl-lib functions

--- packages/csl-lib/lib/csl-citation.test.js
+++ packages/csl-lib/lib/csl-citation.test.js
@@ -1,14 +1,13 @@
 var td = require('testdouble');
 var tap = require('tap');
-var citation = require('./csl-citation'); // Subject Under Test
+var citationFn = require('./csl-citation'); // Subject Under Test
+
+var citation = citationFn({} /*locales*/, {} /* macros */, {} /* attributes */);
 
 tap.test('invoke children with `refs`', t => {
   var refs = [];
   var foo = td.function();
-
-  citation([], {}, [foo], refs);
-
+  citation([foo], refs);
   td.verify(foo(refs));
-
   t.end();
 });
--- packages/csl-lib/lib/csl-layout.js
+++ packages/csl-lib/lib/csl-layout.js
@@ -3,7 +3,7 @@ var formattingAttr = require('./csl-attr-formatting');
 var affixesAttr = require('./csl-attr-affixes');
 var delimiterAttr = require('./csl-attr-delimiter');
 
-module.exports = R.curry(function (locales, attrs, children, refs) {
+module.exports = R.curry(function (locales, macros, attrs, children, refs) {
   return R.pipe(
     R.map(R.pipe(R.of, R.ap(children), R.join(''))),
     R.filter(R.complement(R.isEmpty)),
