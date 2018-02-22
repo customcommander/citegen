9c6999d955161730a9d53c94d17aece91ef544ee
customcommander <hello@spinjs.com>
2018-02-22 00:02:04 +0000

    implements csl-l10n-text in csl-lib, no support outside

---
+++ packages/csl-lib/lib/csl-l10n-text.js
@@ -0,0 +1,30 @@
+var R = require('ramda');
+
+var getL10nTermKey = R.always('name');
+var getL10nFormKey = R.always('form');
+var getL10nPluralKey = R.pipe(
+  R.prop('plural'),
+  R.ifElse(
+    R.equals('true'),
+    R.always('multiple'),
+    R.always('single')
+  )
+);
+
+var filterTerm = function (attrs) {
+  return R.where(
+    R.converge(R.unapply(R.fromPairs), [
+      R.converge(R.pair, [getL10nTermKey, R.compose(R.equals, R.prop('term'))]),
+      R.converge(R.pair, [getL10nFormKey, R.compose(R.equals, R.prop('form'))]),
+      R.converge(R.pair, [getL10nPluralKey, R.always(R.is(String))])
+    ])(attrs)
+  );
+}
+
+module.exports = R.curry(function (locales, attrs) {
+  return R.pipe(
+    R.path(['terms', R.prop('term', attrs)]),
+    R.find(filterTerm(attrs)),
+    R.ifElse(R.isNil, R.always(''), R.prop(getL10nPluralKey(attrs)))
+  )(locales);
+});
---
+++ packages/csl-lib/lib/csl-l10n-text.test.js
@@ -0,0 +1,32 @@
+var tap = require('tap');
+var l10nText = require('./csl-l10n-text');
+
+var l10nTextWithLocales = l10nText({
+  terms: {
+    developer: [
+      {
+        name: 'developer',
+        single: 'developer',
+        multiple: 'developers'
+      },
+      {
+        name: 'developer',
+        form: 'short',
+        single: 'dev',
+        multiple: 'devs'
+      }
+    ]
+  }
+});
+
+tap.equal(l10nTextWithLocales({term: 'developer'}), 'developer',
+  'should return the single version by default');
+
+tap.equal(l10nTextWithLocales({term: 'developer', plural: 'true'}), 'developers',
+  'should return the plural version when requested');
+
+tap.equal(l10nTextWithLocales({term: 'developer', form: 'short'}), 'dev',
+  'should return the short single version by default');
+
+tap.equal(l10nTextWithLocales({term: 'developer', form: 'short', plural: 'true'}), 'devs',
+  'should return the short plural version when requested');
