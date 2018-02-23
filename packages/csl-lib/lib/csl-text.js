2ec0df57edfaf75b64e0151507abf12184519c92
customcommander <hello@spinjs.com>
2018-02-23 11:29:03 +0000

    csl-text: add support for macro, term and variable attributes

--- packages/csl-lib/lib/csl-text.js
+++ packages/csl-lib/lib/csl-text.js
@@ -1,5 +1,33 @@
 var R = require('ramda');
+var l10nText = require('./csl-l10n-text');
+
+var getEmptyString = R.always('');
+var getTerm = l10nText;
+var getValue = R.prop('value');
+
+var getVariable = R.curry(function (ref, attrs) {
+  return R.pipe(
+    R.prop('variable'),
+    R.prop(R.__, ref)
+  )(attrs);
+});
+
+var runMacro = R.curry(function (macros, ref, attrs) {
+  return R.pipe(
+    R.prop('macro'),
+    R.propOr(getEmptyString, R.__, macros),
+    R.applyTo(ref)
+  )(attrs);
+});
 
 module.exports = R.curry(function (locales, macros, attrs, children, ref) {
-  return R.prop('value', attrs);
+  var content = R.cond([
+    [R.has('term'), getTerm(locales)],
+    [R.has('variable'), getVariable(ref)],
+    [R.has('macro'), runMacro(macros, ref)],
+    [R.has('value'), getValue],
+    [R.T, getEmptyString]
+  ])(attrs);
+
+  return content;
 });
--- packages/csl-lib/lib/csl-text.test.js
+++ packages/csl-lib/lib/csl-text.test.js
@@ -1,4 +1,39 @@
 var tap = require('tap');
-var text = require('./csl-text'); // Subject Under Test
+var td = require('testdouble');
+var textFn = require('./csl-text'); // Subject Under Test
 
-tap.is(text({}, {}, {value: 'hello'}, [], {}), 'hello', 'should return the value of the `value` attribute');
+tap.test('should support a "value" attribute', t => {
+  var text = textFn({}, {});
+  t.is(text({value: 'hello'}, [], {}), 'hello');
+  t.end();
+});
+
+tap.test('should support a "variable" attribute', t => {
+  var text = textFn({}, {});
+  t.is(text({variable: 'message'}, [], {message: 'hello'}), 'hello');
+  t.end();
+});
+
+tap.test('should support a "macros" attribute', t => {
+  var person = {name: 'John Doe'};
+
+  var sayHello = td.function();
+  td.when(sayHello(person)).thenReturn(`Hello ${person.name}!`);
+
+  var text = textFn(/* locales */{}, /* macros */{greetings: sayHello});
+  t.is(text({macro: 'greetings'}, [], person), 'Hello John Doe!');
+  t.end();
+});
+
+tap.test('should support a "term" attribute', t => {
+  var text = textFn(/* locales */{
+    terms: {
+      greetings: [{
+        name: 'greetings',
+        single: 'Hello World!'
+      }]
+    }
+  }, /* macros */{});
+  t.is(text({term: 'greetings'}, [], {}), 'Hello World!');
+  t.end();
+});
