1176fe0b4f8fc5e951828c707cdaafb00de99089
customcommander <hello@spinjs.com>
2018-02-27 09:41:05 +0000

    refactor csl-node-layout to use transducers

--- packages/csl-lib/lib/csl-node-layout.js
+++ packages/csl-lib/lib/csl-node-layout.js
@@ -3,10 +3,19 @@ var formattingAttr = require('./csl-attr-formatting');
 var affixesAttr = require('./csl-attr-affixes');
 var delimiterAttr = require('./csl-attr-delimiter');
 
+/**
+ * Reducing function
+ * @function
+ */
+var layoutIterator = R.useWith(R.concat, [
+  R.identity,
+  R.ifElse(R.isEmpty, R.always([]), R.of)
+]);
+
 module.exports = R.curry(function (locales, macros, attrs, children, refs) {
+  var transducer = R.compose(R.map(R.of), R.map(R.ap(children)), R.map(R.join('')));
   return R.pipe(
-    R.map(R.pipe(R.of, R.ap(children), R.join(''))),
-    R.filter(R.complement(R.isEmpty)),
+    R.transduce(transducer, layoutIterator, []),
     delimiterAttr(attrs),
     formattingAttr(attrs),
     affixesAttr(attrs)
