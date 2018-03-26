adce2a22d2029829c60a1ac865698e971b34a779
customcommander <hello@spinjs.com>
2018-03-26 09:29:35 +0100

    csl-lib: refactor tests for affixes

--- packages/csl-lib/lib/csl-attr-affixes.test.js
+++ packages/csl-lib/lib/csl-attr-affixes.test.js
@@ -1,14 +1,17 @@
-var tap = require('tap');
-var affixes = require('./csl-attr-affixes'); // Subject Under Test
+const tap = require('tap');
+const affixes = require('./csl-attr-affixes'); // Subject Under Test
 
-tap.equal(affixes(null, 'foo'), 'foo',
-  'default affixes are empty strings');
+tap.equal(affixes(null, '123'), '123',
+  'return value as is if `attrs` is not an object.');
 
-tap.equal(affixes({prefix: '('}, 'foo'), '(foo', 
-  'render `prefix` before `str`');
+tap.equal(affixes({}, '123'), '123',
+  'return value as is if `attrs` does not include affixes attributes.');
 
-tap.equal(affixes({suffix: ')'}, 'foo'), 'foo)',
-  'render `suffix` after `str`');
+tap.equal(affixes({prefix: '('}, '123'), '(123',
+  'prepend `prefix` to value.');
 
-tap.equal(affixes({prefix: '(', suffix: ')'}, 'foo'), '(foo)',
-  'wrap `str` with affixes if both are provided');
+tap.equal(affixes({suffix: ')'}, '123'), '123)',
+  'append `suffix` to value.');
+
+tap.equal(affixes({prefix: '(', suffix: ')'}, '123'), '(123)',
+  'wrap value with `prefix` and `suffix` when both are provided.');
