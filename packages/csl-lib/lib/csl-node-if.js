21b717f342832c4803f834421055104a2ba44f68
customcommander <hello@spinjs.com>
2018-03-27 23:07:02 +0100

    csl-lib: fix support for match attribute in <if>

--- packages/csl-lib/lib/csl-node-if.js
+++ packages/csl-lib/lib/csl-node-if.js
@@ -1,38 +1,40 @@
 var R = require('ramda');
 
-var isType = R.curry(function (types, ref) {
-  var thisType = R.equals(R.prop('type', ref));
-  return R.any(thisType, types);
-});
-
-var isNumeric = R.curry(function (vars, ref) {
-  return R.any(R.pipe(
-    R.prop(R.__, ref),
-    R.split(/[,\-&]/),
-    R.all(R.pipe(R.trim, R.test(/^[a-z]*\d+[a-z]*$/i)))
-  ), vars);
-});
-
-/**
- * @function
- * @param {object} attrs - CSL Node Attributes
- * @return {function[]}
- */
-var generateConditions = R.compose(R.values, R.evolve({
-  type: R.compose(isType, R.split(' ')),
-  'is-numeric': R.compose(isNumeric, R.split(' '))
-}));
-
-var generateEvaluate = function (attrs) {
-  return R.cond([
-    [R.propEq('match', 'any'), R.always(R.any)],
-    [R.propEq('match', 'none'), R.always(R.none)],
-    [R.T, R.always(R.all)]
-  ])(attrs)(R.equals(true));
+var isType = function (ref) {
+  return R.equals(R.prop('type', ref));
 };
 
+var isNumeric = function (ref) {
+  return R.pipe(
+    R.prop(R.__, ref),
+    R.split(/[,\-&]/),
+    R.all(R.pipe(
+      R.trim,
+      R.test(/^[a-z]*\d+[a-z]*$/i)
+    ))
+  );
+};
+
+var generateEvaluate = function (attrs) {
+  var isTrue = R.equals(true);
+  return R.cond([
+    [R.propEq('match', 'any'), R.always(R.any(R.any(isTrue)))],
+    [R.propEq('match', 'none'), R.always(R.all(R.none(isTrue)))],
+    [R.T, R.always(R.all(R.all(isTrue)))]
+  ])(attrs);
+};
+
+var evaluateConditions = function (attrs, ref) {
+  return R.values(
+    R.evolve({
+      'type': R.o(R.map(isType(ref)), R.split(' ')),
+      'is-numeric': R.o(R.map(isNumeric(ref)), R.split(' '))
+    }, R.omit(['match'], attrs))
+  );
+}
+
 module.exports = R.curry(function (locales, macros, attrs, children, ref) {
-  var conditions = generateConditions(attrs);
+  var results = evaluateConditions(attrs, ref);
   var evaluate = generateEvaluate(attrs);
-  return evaluate(R.juxt(conditions)(ref)) ? R.into('', R.map(R.applyTo(ref)), children) : '';
+  return evaluate(results) ? R.into('', R.map(R.applyTo(ref)), children) : '';
 });
