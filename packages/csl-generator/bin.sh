897aeb1b34d0e683f5c24e99c4583ab4ad141403
customcommander <hello@spinjs.com>
2018-07-29 14:13:00 +0100

    csl-generator: fix typo in the exit code in the package binary

--- packages/csl-generator/bin.sh
+++ packages/csl-generator/bin.sh
@@ -6,7 +6,7 @@ xsl_path=$(dirname $(realpath $0))/lib/module.xsl
 # if csl file exists and is not empty
 if [ -s $csl_file ]; then
   xsltproc $xsl_path $csl_file
-  exit $0
+  exit 0
 else
   echo "file not found: $csl_file" 1>&2
   exit 1
