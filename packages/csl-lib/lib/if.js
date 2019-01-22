18d66601e8960f78a92234e08ea4b8a98c716fbd
customcommander <hello@spinjs.com>
2019-01-22 09:52:38 +0000

    refactor: <if>: import Ramda functions individually, add some ES6 constructs and simplify existing code

--- packages/csl-lib/lib/if.js
+++ packages/csl-lib/lib/if.js
@@ -1,30 +1,158 @@
-var R = require('ramda');
-var {isNumeric} = require('./utils/number');
+/**
+ * @license
+ * Copyright (c) 2019 Julien Gonzalez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-var isType = function (ref) {
-  return R.equals(R.prop('type', ref));
-};
+const {
+  all,
+  always,
+  any,
+  applyTo,
+  call,
+  compose,
+  cond,
+  converge,
+  curry,
+  curryN,
+  equals,
+  evolve,
+  flip,
+  identity,
+  join,
+  map,
+  none,
+  omit,
+  propEq,
+  propOr,
+  split,
+  T,
+  useWith,
+  values
+} = require('ramda');
 
-var generateEvaluate = function (attrs) {
-  var isTrue = R.equals(true);
-  return R.cond([
-    [R.propEq('match', 'any'), R.always(R.any(R.any(isTrue)))],
-    [R.propEq('match', 'none'), R.always(R.all(R.none(isTrue)))],
-    [R.T, R.always(R.all(R.all(isTrue)))]
-  ])(attrs);
-};
+const {isNumeric} = require('./utils/number');
 
-var evaluateConditions = function (attrs, ref) {
-  return R.values(
-    R.evolve({
-      'type': R.o(R.map(isType(ref)), R.split(' ')),
-      'is-numeric': R.o(R.map(R.pipe(R.propOr('', R.__, ref), isNumeric)), R.split(' '))
-    }, R.omit(['match'], attrs))
-  );
-}
+const isTrue = equals(true);
 
-module.exports = R.curry(function (locales, macros, attrs, children, ref) {
-  var results = evaluateConditions(attrs, ref);
-  var evaluate = generateEvaluate(attrs);
-  return evaluate(results) ? R.into('', R.map(R.applyTo(ref)), children) : '';
+/**
+ * Applies given predicate to each value in a space-separated-values string,
+ * and collects the result into an array.
+ *
+ * @example
+ * check(equals('foo'), 'foo bar foo');
+ * //=> [true, false, true]
+ *
+ * @function
+ * @param {function} predicate
+ * @param {string} values
+ * @return {boolean}
+ */
+const check = useWith(map, [identity, split(' ')]);
+
+/**
+ * Checks that `ref.type` is set to given `type`.
+ *
+ * @example
+ * checkType({type: 'journal'}, 'journal');
+ * //=> true
+ * checkType({type: 'journal'}, 'book');
+ * //=> false
+ *
+ * @function
+ * @param {object} ref
+ * @param {string} type
+ * @return {boolean}
+ */
+const checkType = flip(propEq('type'));
+
+/**
+ * Checks that given `key` in `ref` holds valid numeric content.
+ *
+ * @example
+ * checkNumeric({edition: '2nd'}, 'edition');
+ * //=> true
+ * checkNumeric({edition: 'foo bar'}, 'edition');
+ * //=> false
+ *
+ * @function
+ * @param {object} ref
+ * @param {string} key A key in ref
+ * @see {@link https://docs.citationstyles.org/en/master/specification.html#choose}
+ *  for how to check if something is numeric
+ */
+const checkNumeric = curryN(2, compose(isNumeric, flip(propOr(''))));
+
+/**
+ * @function
+ * @param {object} attrs
+ * @return {function}
+ */
+const evaluateFunction = cond([
+  [propEq('match', 'any'),
+    always(any(any(isTrue)))],
+  [propEq('match', 'none'),
+    always(all(none(isTrue)))],
+  [T,
+    always(all(all(isTrue)))]]);
+
+/**
+ * Removes any non-constraint keys from `attrs`.
+ *
+ * @function
+ * @param {object} attrs
+ * @return {object}
+ */
+const pickConstraints = omit(['match']);
+
+/**
+ * Returns an object of predicates
+ * @function
+ * @param {object} ref
+ * @return {object}
+ */
+const buildConstraints = ref => ({
+  'type': check(checkType(ref)),
+  'is-numeric': check(checkNumeric(ref))
 });
+
+/**
+ * @function
+ * @param {object} attrs
+ * @param {object} ref
+ * @return {boolean[][]}
+ */
+const evaluateConstraints = useWith(
+  compose(values, flip(evolve)), [
+    pickConstraints,
+    buildConstraints]);
+
+const evaluate = converge(call, [evaluateFunction, evaluateConstraints]);
+
+/**
+ * @function
+ * @param {locale[]} locales
+ * @param {object} macros
+ * @param {object} attrs
+ * @param {function[]} children
+ * @param {object}
+ */
+module.exports = curry((locales, macros, attrs, children, ref) =>
+  evaluate(attrs, ref) ? join('', map(applyTo(ref), children)) : '');
