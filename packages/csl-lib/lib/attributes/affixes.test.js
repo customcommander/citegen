b76b6c609aad148e980fb8a66c932c0da70cc10d
customcommander <hello@spinjs.com>
2019-03-03 10:16:38 +0000

    fix: do not apply affixes on empty strings. closes #89

--- packages/csl-lib/lib/attributes/affixes.test.js
+++ packages/csl-lib/lib/attributes/affixes.test.js
@@ -1,17 +1,11 @@
 const tap = require('tap');
 const affixes = require('./affixes'); // Subject Under Test
 
-tap.equal(affixes(null, '123'), '123',
-  'return value as is if `attrs` is not an object.');
+tap.equal(affixes({}, 'aaa'), 'aaa',
+  'default affixes are empty strings');
 
-tap.equal(affixes({}, '123'), '123',
-  'return value as is if `attrs` does not include affixes attributes.');
+tap.equal(affixes({prefix: '(', suffix: ')'}, 'aaa'), '(aaa)',
+  'apply affixes on non-empty string');
 
-tap.equal(affixes({prefix: '('}, '123'), '(123',
-  'prepend `prefix` to value.');
-
-tap.equal(affixes({suffix: ')'}, '123'), '123)',
-  'append `suffix` to value.');
-
-tap.equal(affixes({prefix: '(', suffix: ')'}, '123'), '(123)',
-  'wrap value with `prefix` and `suffix` when both are provided.');
+tap.equal(affixes({prefix: '(', suffix: ')'}, ''), '',
+  'does not apply affixes on empty string');
