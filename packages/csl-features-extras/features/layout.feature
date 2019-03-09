ed529c4326d65055e6236770224c8dedb6dae40f
customcommander <hello@spinjs.com>
2019-03-09 09:08:57 +0000

    refactor: <layout> ES6 syntax + refactor logic for rendering output. closes #101

---
+++ packages/csl-features-extras/features/layout.feature
@@ -0,0 +1,29 @@
+Feature: Layout
+
+Scenario: Basic Layout Test
+  Given the following citation style
+    """
+    <style class="note" version="1.0" xmlns="http://purl.org/net/xbiblio/csl">
+      <info>
+        <id/>
+        <title/>
+        <updated>2008-10-29T21:01:24+00:00</updated>
+      </info>
+      <citation>
+        <layout delimiter=", " prefix="(" suffix=")" text-decoration="underline">
+          <text variable="title"/>
+        </layout>
+      </citation>
+    </style>
+    """
+  And the following data
+    """
+    [
+      {"type":"book", "title": "hello"},
+      {"type":"chapter", "title": "world"}
+    ]
+    """
+  Then the following result is expected
+    """
+    (<span style="text-decoration:underline">hello, world</span>)
+    """
--- packages/csl-lib/lib/layout.js
+++ packages/csl-lib/lib/layout.js
@@ -1,23 +1,73 @@
-var R = require('ramda');
-var formattingAttr = require('./attributes/formatting');
-var affixesAttr = require('./attributes/affixes');
-var delimiterAttr = require('./attributes/delimiter');
+/**
+ * @license
+ * Copyright (c) 2019 Julien Gonzalez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+const {
+  applyTo,
+  compose,
+  converge,
+  curry,
+  into,
+  isEmpty,
+  map,
+  pipe,
+  reject,
+  unary
+} = require('ramda');
+
+const formatting = require('./attributes/formatting');
+const affixes = require('./attributes/affixes');
+const delimiter = require('./attributes/delimiter');
+
+const attributes = converge(
+  pipe, [
+    unary(delimiter),
+    unary(formatting),
+    unary(affixes)]);
 
 /**
- * Reducing function
- * @function
+ * Render given reference by apply to its children.
+ * @param {function[]} children
+ * @param {object} ref
+ * @return {string}
  */
-var layoutIterator = R.useWith(R.concat, [
-  R.identity,
-  R.ifElse(R.isEmpty, R.always([]), R.of)
-]);
+const applyChildren = curry((children, ref) =>
+  into('', compose(map(applyTo(ref))), children));
 
-module.exports = R.curry(function (locales, macros, attrs, children, refs) {
-  var transducer = R.compose(R.map(R.of), R.map(R.ap(children)), R.map(R.join('')));
-  return R.pipe(
-    R.transduce(transducer, layoutIterator, []),
-    delimiterAttr(attrs),
-    formattingAttr(attrs),
-    affixesAttr(attrs)
-  )(refs);
-});
+/**
+ * Renders all references by applying each to all children.
+ * @param {function[]} children
+ * @param {object[]} refs
+ * @return {array}
+ */
+const render = (children, refs) =>
+  into([], compose(map(applyChildren(children)), reject(isEmpty)), refs);
+
+/**
+ * @param {locale[]} locales
+ * @param {object} macros
+ * @param {attrs} attrs
+ * @param {function[]} children
+ * @param {object[]} refs
+ */
+module.exports = curry((locales, macros, attrs, children, refs) =>
+  attributes(attrs)(render(children, refs)));
