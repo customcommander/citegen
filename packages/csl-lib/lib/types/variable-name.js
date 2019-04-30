086cb71a4735d337a47592fc489b365df093daf6
customcommander <hello@spinjs.com>
2019-04-30 18:53:38 +0100

    chore: define what a name variable looks like

---
+++ packages/csl-lib/lib/types/variable-name.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {object} NameVariable
+ * @property {?string} `given`
+ * @property {?string} `dropping-particle`
+ * @property {?string} `non-dropping-particle`
+ * @property {?string} `family`
+ * @property {?string} `suffix`
+ */
